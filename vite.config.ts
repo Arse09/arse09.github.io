da37e44ffd4d909061f3e404b8ea8fd9e1d0df39
Arse09 <arse.dc.09@gmail.com>
2025-03-03 19:44:29 +0100

    update vite.config.ts build config

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,15 @@ export default defineConfig({
       react(),
       tailwindcss()
   ],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]"
+      }
+    }
+  }
 })
